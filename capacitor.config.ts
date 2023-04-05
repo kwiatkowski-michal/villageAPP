ea88ca9895b89cc4a3d1e9088d1de28ec95561e3
Michał Kwiatkowski <kwiatkowskimichal04@gmail.com>
2023-04-05 21:41:16 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'village.app.ionic7',
+  appName: 'village-app-ionic7',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
