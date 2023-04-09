43d2a592de736e739fdadf31e01e860cab395445
kwiatkowski-michal <kwiatkowskimichal04@gmail.com>
2023-04-09 16:59:43 +0200

    button test

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,30 +1,54 @@
-import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonActionSheet, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import Opis from '../components/Description';
 import { ellipsisHorizontal, ellipsisVertical, handLeftOutline, homeOutline } from 'ionicons/icons';
 import './MainPage.css';
 const StronaGlowna: React.FC = () => {
+  function refreshPage() {
+    window.location.reload();
+  }
   return (
+    <>
     <IonPage>
       <IonHeader>
         <IonToolbar>
-        <IonButtons slot="primary">
-          <IonButton>
-          <IonIcon slot="icon-only" lazy={true} ios={ellipsisHorizontal} md={ellipsisVertical}></IonIcon>
-          </IonButton>
-        </IonButtons>
           <IonTitle>Strona główna</IonTitle>
+          <IonButtons slot="primary">
+              <IonButton id='open-action-sheet'>
+                <IonIcon slot="icon-only" lazy={true} ios={ellipsisHorizontal} md={ellipsisVertical}></IonIcon>
+              </IonButton>
+            </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen={true}>
         <IonHeader collapse="condense">
           <IonToolbar>
-                  <IonTitle size="large" >Strona główna</IonTitle>
+            <IonTitle size="large" >Strona główna</IonTitle>
           </IonToolbar>
         </IonHeader>
         <Opis />
       </IonContent>
     </IonPage>
+    <IonActionSheet
+    trigger="open-action-sheet"
+    header="Opcje"
+    buttons={[
+      {
+        text: 'Sprawdź aktualizacje',
+        data: {
+          action: {refreshPage}
+        }
+      },
+      {
+        text: 'Wyjdź',
+        role: 'cancel',
+        data: {
+          action: 'cancel'
+        }
+      }
+    ]}
+  ></IonActionSheet>
+  </>
   );
 };
 
